5a57522311dc2cb2134ddc883bbb49494ec308a4
Garbi93 <iseung2093@gmail.com>
2023-11-27 09:31:43 +0900

    initial commit

---
+++ src/main/java/com/example/travelback/TravelBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/travelback/TravelBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
