7951dccdd66851d220c0907b53dcdaf07d39ffb4
LeeJungHoon <gns14585@naver.com>
2023-12-01 14:14:48 +0900

    gns14585/카카오 로그인 작업중 - 로그인완료, 세션 저장이 안됨

---
+++ src/main/java/com/example/travelback/user/controller/KakaoAPI.java
@@ -0,0 +1,110 @@
+package com.example.travelback.user.controller;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.springframework.beans.factory.annotation.Value;
+
+public class KakaoAPI {
+
+    public String getAccessToken(String code) {
+        String accessToken = "";
+        String refreshToken = "";
+        String reqURL = "https://kauth.kakao.com/oauth/token";
+
+        try {
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+            StringBuilder sb = new StringBuilder();
+            sb.append("grant_type=authorization_code");
+            sb.append("&client_id=9095ec9b24d94c371bae12f6439455f2");
+            sb.append("&redirect_uri=http://localhost:3000/auth");
+            sb.append("&code=").append(code);
+
+            bw.write(sb.toString());
+            bw.flush();
+
+            int responseCode = conn.getResponseCode();
+            System.out.println("response code = " + responseCode);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            String line;
+            StringBuilder result = new StringBuilder();
+            while ((line = br.readLine()) != null) {
+                result.append(line);
+            }
+            System.out.println("response body=" + result);
+
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(result.toString());
+
+            accessToken = element.getAsJsonObject().get("access_token").getAsString();
+
+            br.close();
+            bw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return accessToken;
+    }
+
+    public HashMap<String, Object> getUserInfo(String accessToken) {
+        HashMap<String, Object> userInfo = new HashMap<>();
+        String reqUrl = "https://kapi.kakao.com/v2/user/me";
+        try {
+            URL url = new URL(reqUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+
+            int responseCode = conn.getResponseCode();
+            System.out.println("responseCode =" + responseCode);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            StringBuilder result = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                result.append(line);
+            }
+            System.out.println("response body =" + result);
+
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(result.toString());
+
+            JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
+            JsonObject kakaoAccount = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
+
+            String nickname = properties.has("nickname")
+                    ? properties.get("nickname").getAsString()
+                    : null;
+
+            String email = kakaoAccount.has("email")
+                    ? kakaoAccount.get("email").getAsString()
+                    : null;
+
+            userInfo.put("nickname", nickname);
+            userInfo.put("email", email);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return userInfo;
+    }
+}
+
