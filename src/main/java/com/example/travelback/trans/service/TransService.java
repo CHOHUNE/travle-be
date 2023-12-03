049d4f3bc15eac79bf8fef1e037542c64218c940
Garbi93 <iseung2093@gmail.com>
2023-12-04 03:18:11 +0900

    12/4 transMainImage 업로드 시 로컬에 파일 저장 하도록 코드 작성 및 throw Execption 하도록 수정 하고 @Transactionanl 어노테이션을 붙여 로직 실행중 하나라도 실패하면 작동하지 않도록 코드 작성

--- src/main/java/com/example/travelback/trans/service/TransService.java
+++ src/main/java/com/example/travelback/trans/service/TransService.java
@@ -6,18 +6,22 @@ import com.example.travelback.trans.mapper.TransMapper;
 import com.example.travelback.trans.mapper.TransTypeMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(rollbackFor = Exception.class)
 public class TransService {
     private final TransMapper mapper;
     private final TransTypeMapper transTypeMapper;
     private final MainImageMapper mainImageMapper;
 
-    public void add(Trans trans, String type, MultipartFile transMainImage) {
+    public void add(Trans trans, String type, MultipartFile transMainImage) throws IOException {
         // 상품 추가
         mapper.insert(trans);
 
@@ -25,13 +29,30 @@ public class TransService {
         // -> transport(tId), 이미지 파일 이름
         if(transMainImage != null) { // 파일을 넣었을때에만 
             mainImageMapper.insert(trans.getTId(), transMainImage.getOriginalFilename());
+            // 파일이 실제로 등록이 된 후에는 S3로 저장 기능 추가
+            // 실제 파일을 S3 bucket에 upload (파일이 있으면 )
+            upload(trans.getTId(), transMainImage);
         }
-
-        // 실제 파일을 S3 bucket에 upload (파일이 있으면 )
 
         // 상품 등록할 때에 상품 타입을 등록 하는 기능
         transTypeMapper.insert(trans.getTId(), type);
     }
+
+    // 로컬에 테스트 (시작) ----------------------------------------------------------------------------------------------
+    private void upload(Integer tId, MultipartFile transMainImage) throws IOException {
+        // 파일저장 경로
+        // /Users/seungwon/Desktop/study/Temp/travel/trans/mainImage/게시물번호/파일명
+        File folder = new File("/Users/seungwon/Desktop/study/Temp/travel/trans/mainImage/"+tId);
+        if(!folder.exists()) {
+            folder.mkdirs();
+        }
+
+        String path = folder.getAbsolutePath() + "/" + transMainImage.getOriginalFilename();
+        File des = new File(path);
+        transMainImage.transferTo(des);
+
+    }
+    // 로컬에 테스트 (끝) ----------------------------------------------------------------------------------------------
 
     public List<Trans> list() {
         return mapper.selectAll();
