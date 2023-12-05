c1c0e3247d7a5e6b3662b7b63091dd6b4008915c
Garbi93 <iseung2093@gmail.com>
2023-12-05 11:03:02 +0900

    12/05 운송 상품의 상품설명 이미지 dto 작성

---
+++ src/main/java/com/example/travelback/trans/dto/TransContentImages.java
@@ -0,0 +1,10 @@
+package com.example.travelback.trans.dto;
+
+import lombok.Data;
+
+@Data
+public class TransContentImages {
+    private Integer id;
+    private String name;
+    private String url;
+}
