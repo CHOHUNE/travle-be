220bf1039bc36e507b8eb496b4f01e848da0e192
Garbi93 <iseung2093@gmail.com>
2023-12-17 18:19:42 +0900

    12/17 운송 결제 하게 되면 저장 되는 코드 수정 db도 추가

--- src/main/java/com/example/travelback/toss/mapper/TossMapper.java
+++ src/main/java/com/example/travelback/toss/mapper/TossMapper.java
@@ -44,25 +44,27 @@ public interface TossMapper {
         """)
     List<Toss> getId(String userId);
 
+    @Insert("""
+            insert into transtosspay (
+                transId,
+                amount,
+                orderId,
+                requested, 
+                realUserPhoneNumber,
+                userId
+                ) 
+            values (
+                #{id},
+                #{amount},
+                #{orderId},
+                #{requested},
+                #{phoneNumber},
+                #{userId}
+                );
+        """)
+    int transSave(Integer id, Integer amount, String orderId, String requested, String phoneNumber, String userId);
 
-//    // 운송 상품 결제 시 db저장 기능
-//    @Insert("""
-//         INSERT INTO transtosspay (
-//            orderId,
-//            userId,
-//            userName,
-//            realUserName,
-//            realUserPhoneNumber,
-//            people,
-//            transId,
-//            transTitle,
-//            transStartDay,
-//            amount,
-//            requested,
-//            inserted
-//         ) VALUES (
-//         )
-//    """)
-//    int save();
+
+
 }
 
