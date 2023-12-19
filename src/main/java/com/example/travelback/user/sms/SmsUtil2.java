cdc4078815bf54945a9f4183b7f9ca66c90eb60b
LeeJungHoon <gns14585@naver.com>
2023-12-19 16:11:42 +0900

    gns14585 / commit

--- src/main/java/com/example/travelback/user/sms/SmsUtil2.java
+++ src/main/java/com/example/travelback/user/sms/SmsUtil2.java
@@ -39,9 +39,32 @@ public class SmsUtil2 {
     }
 
 
-    // ---------- 문자 발송 로직 ----------
+    // ---------- 문자 발송 로직 운송상품용 ----------
     @PostMapping("sendSMS3")
     public ResponseEntity<String> sendOne(@RequestParam("userPhoneNumber") String phoneNumber, @RequestBody Member member) {
+
+        System.out.println("전화번호: " + phoneNumber);
+        System.out.println("예약번호: " + member.getMessageContent());
+
+        if (messageService == null) {
+            throw new IllegalStateException("초기화되었는지 확인");
+        }
+
+        Message message = new Message();
+        message.setFrom("01036138304"); // 발신자 번호
+        message.setTo(phoneNumber); // 수신자 번호: 사용자가 입력한 번호
+        // 메시지 내용: 판매자가 입력한 예약번호
+        message.setText("[Web발신]\n트레블 여행\n구입하신 상품의 예약번호\n[" + member.getMessageContent() + "] 입니다.");
+
+        SingleMessageSentResponse response = this.messageService.sendOne(new SingleMessageSendingRequest(message));
+        System.out.println(response);
+
+        return ResponseEntity.ok().build();
+    }
+
+    // ---------- 문자 발송 로직 호텔상품용 ----------
+    @PostMapping("sendSMS4")
+    public ResponseEntity<String> sendOne2(@RequestParam("userPhoneNumber") String phoneNumber, @RequestBody Member member) {
 
         System.out.println("전화번호: " + phoneNumber);
         System.out.println("예약번호: " + member.getMessageContent());
